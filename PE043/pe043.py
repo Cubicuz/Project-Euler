2a71e292405f4fbb4c16428212ba0be14e2860f6
michi <michi@pegamenis-PC.speedport.ip>
2018-04-21 16:46:20 +0200

    43 done, description of 46 and 47

--- PE043/pe043.py
+++ PE043/pe043.py
@@ -12,3 +12,38 @@
 #    d8d9d10=289 is divisible by 17
 #
 #Find the sum of all 0 to 9 pandigital numbers with this property.
+
+import itertools
+
+allPandigitalNumbers = itertools.permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+reducedPandigitalNumbers = []
+for numbr in allPandigitalNumbers:
+# reihenfolge der Checks aus performancegründen geändert
+
+#    d3d4d5 divisible by 5
+    if (numbr[5] != 5 and numbr[5] != 0):
+        continue
+#    d1d2d3 divisible by 2
+    if numbr[3] % 2 != 0:
+        continue
+#    d2d3d4 divisible by 3
+    if (numbr[2]+numbr[3]+numbr[4]) % 3 != 0:
+        continue
+#    d7d8d9 divisible by 17
+    if (numbr[7]*100+numbr[8]*10+numbr[9]) % 17 != 0:
+        continue
+#    d6d7d8 divisible by 13
+    if (numbr[6]*100+numbr[7]*10+numbr[8]) % 13 != 0:
+        continue
+#    d5d6d7 divisible by 11
+    if (numbr[5]*100+numbr[6]*10+numbr[7]) % 11 != 0:
+        continue
+#    d4d5d6 divisible by 7
+    if (numbr[4]*100+numbr[5]*10+numbr[6]) % 7 != 0:
+        continue
+    reducedPandigitalNumbers.append(numbr)
+totalSum=0
+for numbr in reducedPandigitalNumbers:
+    for i in range(0, 10):
+        totalSum += numbr[i] * (10**(9-i))
+print(totalSum)
