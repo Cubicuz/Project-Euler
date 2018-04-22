669410d1c52a06fbac5266c8e452918aae2dc28f
michi <michi@pegamenis-PC.speedport.ip>
2018-04-22 09:52:41 +0200

    done 46

--- PE046/pe046.py
+++ PE046/pe046.py
@@ -2,13 +2,39 @@
 #
 #It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a square.
 #
-#9 = 7 + 2×12
-#15 = 7 + 2×22
-#21 = 3 + 2×32
-#25 = 7 + 2×32
-#27 = 19 + 2×22
-#33 = 31 + 2×12
+#9 = 7 + 2×1^2
+#15 = 7 + 2×2^2
+#21 = 3 + 2×3^2
+#25 = 7 + 2×3^2
+#27 = 19 + 2×2^2
+#33 = 31 + 2×1^2
 #
 #It turns out that the conjecture was false.
 #
 #What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
+
+inp=""
+f = open("../stuff/prims below one million.txt","r")
+for line in f:
+    inp+=line
+prims=inp.split()
+primsSet = set()
+for prim in prims:
+    primsSet.add(int(prim))
+print("read")
+cont = 1
+i=13
+while cont==1:
+    i+=2
+    if i in primsSet:
+        continue
+    j=1
+    canBeWritten=0
+    while (i-2*j*j > 0) and (canBeWritten==0):
+        if (i-2*j*j in primsSet):
+            canBeWritten = 1
+        j+=1
+    if canBeWritten == 0:
+        cont = 0
+        print('its this {}'.format(i))
+        
--- stuff/prims below one million.py
+++ stuff/prims below one million.py
@@ -15,3 +15,11 @@ print("done")
 #f = open("prims below one million.txt",'w')
 #f.write(" ".join(prims))
 #f.close()
+
+#reding primes
+inp=""
+f = open("../stuff/prims below one million.txt","r")
+for line in f:
+    inp+=line
+prims=inp.split()
+print("read")
